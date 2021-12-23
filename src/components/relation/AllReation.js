c38ec0cd0112ab8d6da6cb79fd07749ca47d9ecc
hanhan207 <785280596@qq.com>
2021-12-23 16:43:41 +0800

    g6

--- src/components/relation/AllReation.js
+++ src/components/relation/AllReation.js
@@ -1,10 +1,17 @@
-import { React, useState, useEffect } from "react";
-import Graphin, { Behaviors, Utils } from "@antv/graphin";
-import { Statistic as NewStat } from "@antv/graphin-components";
-import { Row, Col, Card } from "antd";
+import { React, useState, useEffect, useContext } from "react";
+import Graphin, { Behaviors, Utils, GraphinContext } from "@antv/graphin";
+import {
+  Statistic as NewStat,
+  ContextMenu,
+  Tooltip,
+  FishEye,
+  Legend,
+} from "@antv/graphin-components";
+import { Row, Col, Card, Switch } from "antd";
 import { getAll as IgetAll } from "../../api/Ineo4j";
 
 const { DragCanvas, ZoomCanvas, DragNode, ActivateRelations } = Behaviors;
+const { Menu } = ContextMenu;
 const colors = {
   blue: "rgba(72, 83, 159, 1)",
   red: "rgba(185, 63, 57, 1)",
@@ -14,6 +21,8 @@ const colors = {
 };
 function AllRelation() {
   const [alldata, setAlldata] = useState([]);
+  const [fish, setFish] = useState(false);
+  const [tip, setTip] = useState(false);
 
   useEffect(() => {
     IgetAll().then((res) => {
@@ -37,21 +46,26 @@ function AllRelation() {
       };
       switch (node.labels) {
         case "man":
+          node.legend = "人物";
           mykeyshape.size = 50;
           break;
         case "op":
+          node.legend = "剧院";
           mykeyshape.fill = colors.yellow;
           mykeyshape.stroke = colors.yellow;
           break;
         case "thb":
+          node.legend = "老字号";
           mykeyshape.fill = colors.blue;
           mykeyshape.stroke = colors.blue;
           break;
         case "db":
+          node.legend = "地标";
           mykeyshape.fill = colors.red;
           mykeyshape.stroke = colors.red;
           break;
         case "ht":
+          node.legend = "胡同";
           mykeyshape.fill = colors.green;
           mykeyshape.stroke = colors.green;
           break;
@@ -62,8 +76,11 @@ function AllRelation() {
         // 节点的主要形状，即圆形容器，可以在这里设置节点的大小，border，填充色
         keyshape: mykeyshape,
         label: {
-        //   value: node.labels,
-        value:node.labels === 'man'? node.properties.name:node.properties[`${node.labels}_name`]
+          //   value: node.labels,
+          value:
+            node.labels === "man"
+              ? node.properties.name
+              : node.properties[`${node.labels}_name`],
         },
       };
     });
@@ -83,23 +100,53 @@ function AllRelation() {
     return edges;
   }
   return (
-    <div id="mapBox">
-      <Graphin data={alldata} layout={{ type: "gForce" }}>
-        <ActivateRelations />
-      </Graphin>
+    <div>
+      <Switch
+        checkedChildren="开启鱼眼"
+        unCheckedChildren="关闭鱼眼"
+        onChange={(e) => setFish(e)}
+      />
+      <Switch
+        checkedChildren="显示信息"
+        unCheckedChildren="隐藏信息"
+        onChange={(e) => setTip(e)}
+      />
+      <div id="mapBox">
+        <Graphin data={alldata} layout={{ type: "gForce" }}>
+          <ActivateRelations />
+          {tip && (
+            <Tooltip bindType="node">
+              <CustomTooltip />
+            </Tooltip>
+          )}
+          <FishEye options={{}} visible={fish} />
+          <Legend
+            bindType="node"
+            sortKey="legend"
+            colorKey="style.keyshape.stroke"
+          >
+            <Legend.Node />
+          </Legend>
+        </Graphin>
+      </div>
     </div>
-    //     <Row gutter={16}>
-    //     {/* <Col span={12}>
-    //       <Card title="关系数据" bodyStyle={{ height: '554px', overflow: 'scroll' }}>
-    //         <pre>{JSON.stringify(alldata, null, 2)}</pre>
-    //       </Card>
-    //     </Col> */}
-    //     <Col span={18}>
-    //       <Card title="可视化结果">
-    //       </Card>
-    //     </Col>
-    //   </Row>
   );
 }
+
+const CustomTooltip = () => {
+  const { tooltip } = useContext(GraphinContext);
+  const context = tooltip.node;
+  const { item } = context;
+  const model = item && item.getModel();
+  return (
+    // @ts-ignore
+    <div>
+      <Card title="节点信息" style={{ width: "400px" }}>
+        ID : {model.id}
+        {/* <pre>{JSON.stringify(model.properties, null, 2)}</pre> */}
+      </Card>
+    </div>
+  );
+};
 
 export default AllRelation;
