6afb02af0772dcf555d1cc2ff4050c271fed7367
hanhan207 <785280596@qq.com>
2022-01-12 20:37:04 +0800

    index

--- src/components/playground/Playgroud.js
+++ src/components/playground/Playgroud.js
@@ -15,15 +15,83 @@ import p2 from "../../img/p1/P1_2.png";
 import p3 from "../../img/p1/P1_3.png";
 import p4 from "../../img/p1/P1_4.png";
 
+function SectionHeader({ title }) {
+  return (
+    <h6 style={{ fontWeight: "600", letterSpacing: "1px", fontSize: "30px" }}>
+      {title}
+    </h6>
+  );
+}
+
+function Header() {
+  return (
+    <section className={"header-section"}>
+      <ul className="header-menu">
+        <li>Intro</li>
+        <li>About</li>
+        <li>Featured</li>
+      </ul>
+      <h1 id="header-text">Art Objects</h1>
+    </section>
+  );
+}
+
+function Featured() {
+  return (
+    <section className={"featured-section"}>
+      <div className="feature-row-layout">
+        <h6 className="f6">green</h6>
+        <img className="fimg" src="https://picsum.photos/600/300" alt="" />
+      </div>
+      <div className="feature-column-layout">
+        <h6 className="f6">lily</h6>
+        <img
+          className="fimg"
+          src="https://picsum.photos/800/500"
+          alt=""
+          style={{ height: "125vh" }}
+        />
+      </div>
+    </section>
+  );
+}
+
+function About() {
+  return (
+    <section className={"about-section"}>
+      <SectionHeader title={"about"} />
+      <p id="headline">
+        Your websites and tutorials are awesome and easy to understand!! Just
+        one suggestion though... If possible, please try to add timestamps so
+        that it's easier to keep a track of at what time will you be working on
+        which element.
+      </p>
+    </section>
+  );
+}
+
+function Gallery() {
+  return <section className={"gallery-section"}></section>;
+}
+
+function Footer() {
+  return <section className={"footer-section"}></section>;
+}
+
 function Playground() {
   return (
-    <div>
-      <NavLink to="/">To LandPage</NavLink>
-      {/* <img
-        src="https://a.amap.com/jsapi_demos/static/demo-center/3d_texture_cctv_256.png"
-        alt=""
-      />
-      <img src="http://hanhan.run/mc_img/3d_texture_cctv_256.png" alt="" /> */}
+    <div className="main-container">
+      {/* <NavLink to="/">To LandPage</NavLink> */}
+      <div className="navbar">
+        <div>menu</div>
+        <div>Urban Culture</div>
+        <div>about</div>
+      </div>
+      <Header />
+      <Featured />
+      <About />
+      <Gallery />
+      <Footer />
     </div>
   );
 }
