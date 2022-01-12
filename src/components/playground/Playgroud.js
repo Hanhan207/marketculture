8d109dd4793c378c1a48a543dc31e7aaf514d9e7
hanhan207 <785280596@qq.com>
2022-01-12 12:51:31 +0800

    jd

--- src/components/playground/Playgroud.js
+++ src/components/playground/Playgroud.js
@@ -18,26 +18,12 @@ import p4 from "../../img/p1/P1_4.png";
 function Playground() {
   return (
     <div>
-      <Tilt
-        className="parallax-effect-img"
-        tiltMaxAngleX={10}
-        tiltMaxAngleY={10}
-        perspective={800}
-        transitionSpeed={150}
-        // scale={1.1}
-        gyroscope={true}
-      >
-        <img src={p4} className="inner p4" alt="pic" />
-        <img src={p1} className="inner p1" alt="pic" />
-        <img src={p2} className="inner p1" alt="pic" />
-        <img src={p3} className="inner p1" alt="pic" />
-      </Tilt>
       <NavLink to="/">To LandPage</NavLink>
-      <img
+      {/* <img
         src="https://a.amap.com/jsapi_demos/static/demo-center/3d_texture_cctv_256.png"
         alt=""
       />
-      <img src="http://hanhan.run/mc_img/3d_texture_cctv_256.png" alt="" />
+      <img src="http://hanhan.run/mc_img/3d_texture_cctv_256.png" alt="" /> */}
     </div>
   );
 }
